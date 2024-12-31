ff46515699d193e43d7293c5747989230ae13a05
Thanushan1523 <thanushan1523@gmail.com>
2024-12-31 08:00:34 -0500

    working on logic-operation

--- src/App.jsx
+++ src/App.jsx
@@ -33,24 +33,24 @@ function App() {
       </div>
       <button className='span-two'>AC</button>
       <button >DEL</button>
-      <OperationButton/>
-      <button ></button>
-      <DigitButton operation= "÷" dispatch={dispatch}/>
+  
+     
+      <OperationButton operation= "÷" dispatch={dispatch}/>
       <DigitButton digit= "2" dispatch={dispatch}/>
       <DigitButton digit= "3" dispatch={dispatch}/>
-      <DigitButton operation= "*" dispatch={dispatch}/>
+      <OperationButton operation = "*" dispatch={dispatch}/>
       
       <DigitButton digit= "4" dispatch={dispatch}/>
       <DigitButton digit= "5" dispatch={dispatch}/>
       <DigitButton digit= "6" dispatch={dispatch}/>
-      <DigitButton operation= "+" dispatch={dispatch}/>
+      <OperationButton operation= "+" dispatch={dispatch}/>
       
       <DigitButton digit= "7" dispatch={dispatch}/>
       <DigitButton digit= "8" dispatch={dispatch}/>
       <DigitButton digit= "9" dispatch={dispatch}/>
-      <DigitButton operation= "-" dispatch={dispatch}/>
+      <OperationButton operation= "-" dispatch={dispatch}/>
      
-      <DigitButton digit= "." dispatch={dispatch}/>
+      <OperationButton digit= "." dispatch={dispatch}/>
       <DigitButton digit= "0" dispatch={dispatch}/>
       <button className='span-two'>=</button>
     </div>
